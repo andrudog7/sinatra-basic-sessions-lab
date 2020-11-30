b66572d4b4bdd2348a5f66d906c73ed1d2f3e910
--add <andrewksmoker@gmail.com>
2020-11-30 15:13:36 -0500

    commit tree c1a55d31b6e7147d11a1f96001a2326dcf95e054

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
+configure do 
+    enable :sessions
+    set :session_secret, 'SecureRandom.hex(64)'
+end
+
 class App < Sinatra::Base
+    get '/' do 
+        erb :index 
+    end
+
+    post '/checkout' do 
+        session[:item] = params[:item]
+        @session = session
+        binding.pry
+        erb :checkout
+    end
 end
